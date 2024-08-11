572d5a657039e47f1119409ad9bf54ad732c5e8a
shruti <122924152+shruti-ugale-code@users.noreply.github.com>
2024-08-11 20:11:35 +0530

    Add files via upload
    this file contain count ,arraylist of the all the ans .

--- Letter_Combinations_of_a_Phone_Number.java
+++ Letter_Combinations_of_a_Phone_Number.java
@@ -1,21 +1,66 @@
+import java.util.ArrayList;
+
 public class Letter_Combinations_of_a_Phone_Number {
     public static void main(String[] args) {
-        phon("","12");
+        phon("", "12");
+        System.out.println();
+        System.out.println(phonp("", "12"));
+        System.out.println(phoncount("","12"));
 
 
     }
-    public static void phon(String p,String up){
-        if (up.isEmpty()){
-            System.out.println(p);
+
+    public static void phon(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.print(p+" ");
             return;
 
         }
-        int digit=up.charAt(0)-'0';
-        for (int i = (digit-1)*3; i <digit*3 ; i++) {
-            char ch=(char)('a'+i);
-            phon(p+ch,up.substring(1));
+        int digit = up.charAt(0) - '0';
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+            phon(p + ch, up.substring(1));
 
 
         }
     }
+
+    public static ArrayList<String> phonp(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+
+
+        }
+        int digit1 = up.charAt(0) - '0';
+        ArrayList<String> ans = new ArrayList<>();
+        for (int i = (digit1 - 1) * 3; i < digit1 * 3; i++) {
+            char ch = (char) ('a' + i);
+            ans.addAll(phonp(p + ch, up.substring(1)));
+
+
+        }
+        return ans;
+
+    }
+
+    public static int phoncount(String p, String up) {
+        if (up.isEmpty()) {
+
+            return 1;
+
+        }
+        int digit = up.charAt(0) - '0';
+        int count = 0;
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+          count=count+ phoncount(p + ch, up.substring(1));
+
+
+        }
+return count;
+    }
 }
+
+
